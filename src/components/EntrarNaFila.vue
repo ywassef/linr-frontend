b715459b0965476213bd5e226dcf5b5fe5e1b935
Alex Alves <alexlalves@live.com>
2017-11-22 11:14:10 -0200

    Finally managed to install the damn thing

--- src/components/EntrarNaFila.vue
+++ src/components/EntrarNaFila.vue
@@ -56,6 +56,9 @@
   };
 
   import Vue from 'vue'
+  import '../../serviceworker.js'
+  require('../../serviceworker.js');
+
 
   export default {
     name: 'Entrarnafila',
@@ -80,11 +83,72 @@
 
         }
         else {
+
+          // Substitute this with API calls
+
           Vue.prototype.$name = document.getElementById('NameField').value
           Vue.prototype.$num_people = document.getElementById('NumPeopleField').value
-          Vue.prototype.$rest_id = document.getElementById('restaurantID').textContent
+          //Vue.prototype.$rest_id = document.getElementById('restaurantID').textContent
           Vue.prototype.$line = '15'
           Vue.prototype.$time = '30'
+
+          // Subscribe user to push notifications
+
+          var endpoint;
+          var key;
+          var authSecret;
+
+          //Register a Service Worker.
+
+          navigator.serviceWorker.register('../../serviceworker.js')
+            .then(function(registration) {
+
+              //Use the PushManager to get the user’s subscription to the push service.
+
+              return registration.pushManager.getSubscription()
+                .then(function(subscription) {
+
+                  //If a subscription was found, return it.
+
+                  if (subscription) {
+                    return subscription;
+                  }
+
+                  //Otherwise, subscribe the user (userVisibleOnly allows to specify that we don’t plan to send notifications that don’t have a visible effect for the user).
+
+                  return registration.pushManager.subscribe({ userVisibleOnly: true });
+                });
+            }).then(function(subscription) {
+
+            //Retrieve the user’s public key.
+
+            var rawKey = subscription.getKey ? subscription.getKey('p256dh') : '';
+            key = rawKey ?
+              btoa(String.fromCharCode.apply(null, new Uint8Array(rawKey))) : '';
+            var rawAuthSecret = subscription.getKey ? subscription.getKey('auth') : '';
+            authSecret = rawAuthSecret ?
+              btoa(String.fromCharCode.apply(null, new Uint8Array(rawAuthSecret))) :
+              '';
+
+            endpoint = subscription.endpoint;
+
+            //Send the subscription details to the server using the Fetch API.
+
+            fetch('./register', {
+              method: 'post',
+              headers: {
+                'Content-type': 'application/json'
+              },
+              body: JSON.stringify({
+                endpoint: subscription.endpoint,
+                key: key,
+                authSecret: authSecret,
+              }),
+            });
+          });
+
+
+          // Go to confirmation page
 
           this.$router.push({
             path: 'confirmado',
