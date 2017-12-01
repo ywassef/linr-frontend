2ec9cb6f5ecf128e328cecdba6a7332d1c4f5e6c
yasmin <ywassef@hotmail.com>
2017-12-01 15:05:43 -0200

    Clean code e and fix location according to master

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Entrarnafila from '../components/EntrarNaFila.vue'
+import Entrarnafila from './EntrarNaFila.vue'
 import Home from './Home.vue'
 import Cadastro from './Cadastro.vue'
 import Login from './Login.vue'
