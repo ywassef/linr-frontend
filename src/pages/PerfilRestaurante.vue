e04911eec4143803953ce4c8a54846da9b8d4a59
yasmin <ywassef@hotmail.com>
2017-12-04 11:37:24 -0200

    Página de perfil de restaurante terminada

--- src/pages/PerfilRestaurante.vue
+++ src/pages/PerfilRestaurante.vue
@@ -9,18 +9,14 @@
         </div>
         <div class="media-content">
           <div class="content">
-            <h2><strong>Nome Restaurante</strong></h2>
+            <h2><strong>{{nome}}</strong></h2>
           </div>
           <div class="level is-mobile">
             <div class="level-left">
+
               <a class="level-item">
-                <h3 class="subtitle"><i class="fa fa-users" aria-hidden="true"></i>
-                15
-                </h3>
-              </a>
-              <a class="level-item">
-                <h3 class="subtitle"  style='margin-left:1.0rem'><i class="fa fa-star" aria-hidden="true"></i>
-                4
+                <h3 class="subtitle"><i class="fa fa-star" aria-hidden="true"></i>
+                {{avaliacao}}
               </h3>
               </a>
             </div>
@@ -30,26 +26,33 @@
     </div>
 
     <div class="box has-text-left">
+      <strong>Filas:</strong>
+      <p><small>
+        <span v-html="filas"></span>
+      </small></p>
+    </div>
+
+    <div class="box has-text-left">
       <strong>Descrição:</strong>
       <p style="text-align:justify "><small>
-        Informação adicional. By clicking Create Account, you agree to our Terms and that you have read our Data Policy, including our Cookie Use. You may receive SMS Notifications from Facebook and can opt out at any time.
+        {{informacao_adicional}}
       </small></p>
     </div>
 
     <div class="box has-text-left">
       <strong>Endereço:</strong>
       <p><small>
-        Shopping CenterVale
+        {{endereco}}
       </small></p>
     </div>
 
     <div class="box has-text-left">
       <strong>Contato:</strong>
       <p><small>
-        <strong>Telefone:</strong> (11) 11111-1111
+        <strong>Telefone:</strong> {{telefone}}
       </small></p>
       <p><small>
-        <strong>Site:</strong> <a href="https://www.w3schools.com/html/">www.restaurante.com.br</a>
+        <strong>Site:</strong> <a v-bind:href="'http://' + site">{{site}}</a>
       </small></p>
     </div>
 
@@ -68,8 +71,14 @@
     <div class="box has-text-left">
       <strong>Formas de pagamento:</strong>
       <p><small>
-        Cartão de crédito<br>Cartão de débito<br>Dinheiro
+        <span v-html="pagamento"></span>
       </small></p>
+    </div>
+
+    <div class="hero-body">
+      <button class="button is-large is-primary"
+              v-on:click="entrar_na_fila">ENTRAR NA FILA
+      </button>
     </div>
 
   </section>
@@ -77,9 +86,20 @@
 
 <script>
 
+  var hora_func_full = ''
+  var hora_func_short = ''
+
   var data = {
+    nome: '',
+    avaliacao: 'placeholder',
+    filas: '',
+    informacao_adicional: '',
+    endereco: '',
+    telefone: '',
+    site: '',
     hora_funcionamento: '',
-    botao_icon: ''
+    botao_icon: '',
+    pagamento: ''
   }
 
   import MaskedInput from 'vue-masked-input'
@@ -92,27 +112,224 @@
       return data
     },
     methods: {
-      load_hora_func() {
-        data.hora_funcionamento = '<strong>Hoje:</strong> 10h às 19h'
-        data.botao_icon = 'fa fa-angle-down'
+      entrar_na_fila() {
+        this.$router.push('/')
+      },
+      loadPagina() {
+        hora_func_full = ''
+        hora_func_short = ''
+        const rest_id = this.$route.params.r_id
+        var filas = ''
+        data.filas = ''
+        data.pagamento = ''
+        const vm = this
+
+        vm.$http
+          .get(api(`/restaurantes/${rest_id}`))
+          .then(function (response) {
+            const dados = response.data.data[0];
+            //console.log(dados)
+            data.nome = dados.nome
+            data.informacao_adicional = dados.informacao_adicional
+            data.endereco = dados.endereco
+            data.telefone = dados.telefone
+            data.site = dados.site
+            if(dados.forma_pagamento === null) {
+              //console.log('pagamento: 0')
+            }
+            else {
+              //console.log('pagamento: ' + dados.forma_pagamento.length)
+              for (var i = 0; i < dados.forma_pagamento.length ; i++) {
+                data.pagamento = data.pagamento + dados.forma_pagamento[0] + '<br>'
+              }
+            }
+
+            const d = new Date()
+            const func_ini = dados.hora_funcionamento_inicio
+            const func_fim = dados.hora_funcionamento_fim
+            hora_func_short = '<strong>Hoje: </strong>'
+            switch (d.getDay()) {
+              case 0:
+                if(func_ini[0] === null) {
+                  hora_func_short = 'Não abre hoje'
+                }
+                else {
+                  hora_func_short = hora_func_short + func_ini[0].slice(0, -3) + ' às ' + func_fim[0].slice(0, -3)
+                }
+                break
+              case 1:
+                if(func_ini[1] === null) {
+                  hora_func_short = 'Não abre hoje'
+                }
+                else {
+                  hora_func_short = hora_func_short + func_ini[1].slice(0, -3) + ' às ' + func_fim[1].slice(0, -3)
+                }
+                break
+              case 2:
+                if(func_ini[2] === null) {
+                  hora_func_short = 'Não abre hoje'
+                }
+                else {
+                  hora_func_short = hora_func_short + func_ini[2].slice(0, -3) + ' às ' + func_fim[2].slice(0, -3)
+                }
+                break
+              case 3:
+                if(func_ini[3] === null) {
+                  hora_func_short = 'Não abre hoje'
+                }
+                else {
+                  hora_func_short = hora_func_short + func_ini[3].slice(0, -3) + ' às ' + func_fim[3].slice(0, -3)
+                }
+                break
+              case 4:
+                if(func_ini[4] === null) {
+                  hora_func_short = 'Não abre hoje'
+                }
+                else {
+                  hora_func_short = hora_func_short + func_ini[4].slice(0, -3) + ' às ' + func_fim[4].slice(0, -3)
+                }
+                break
+              case 5:
+                if(func_ini[5] === null) {
+                  hora_func_short = 'Não abre hoje'
+                }
+                else {
+                  hora_func_short = hora_func_short + func_ini[5].slice(0, -3) + ' às ' + func_fim[5].slice(0, -3)
+                }
+                break
+              case 6:
+                if(func_ini[6] === null) {
+                  hora_func_short = 'Não abre hoje'
+                }
+                else {
+                  hora_func_short = hora_func_short + func_ini[6].slice(0, -3) + ' às ' + func_fim[6].slice(0, -3)
+                }
+                break
+              default:
+                break
+            }
+            console.log('hora func short: ' + hora_func_short)
+
+            for (var i = 0; i < 7; i++) {
+              switch(i) {
+                case 0:
+                  hora_func_full = hora_func_full + '<strong>Domingo: </strong>'
+                  if(func_ini[0] === null) {
+                    hora_func_full = hora_func_full + 'Não abre' + '<br>'
+                  }
+                  else {
+                    hora_func_full = hora_func_full + func_ini[0].slice(0, -3) + ' às ' + func_fim[0].slice(0, -3) + '<br>'
+                  }
+                  break
+                case 1:
+                  hora_func_full = hora_func_full + '<strong>Segunda: </strong>'
+                  if(func_ini[1] === null) {
+                    hora_func_full = hora_func_full + 'Não abre' + '<br>'
+                  }
+                  else {
+                    hora_func_full = hora_func_full + func_ini[1].slice(0, -3) + ' às ' + func_fim[1].slice(0, -3) + '<br>'
+                  }
+                  break
+                case 2:
+                  hora_func_full = hora_func_full + '<strong>Terça: </strong>'
+                  if(func_ini[2] === null) {
+                    hora_func_full = hora_func_full + 'Não abre' + '<br>'
+                  }
+                  else {
+                    hora_func_full = hora_func_full + func_ini[2].slice(0, -3) + ' às ' + func_fim[2].slice(0, -3) + '<br>'
+                  }
+                  break
+                case 3:
+                  hora_func_full = hora_func_full + '<strong>Quarta: </strong>'
+                  if(func_ini[3] === null) {
+                    hora_func_full = hora_func_full + 'Não abre' + '<br>'
+                  }
+                  else {
+                    hora_func_full = hora_func_full + func_ini[3].slice(0, -3) + ' às ' + func_fim[3].slice(0, -3) + '<br>'
+                  }
+                  break
+                case 4:
+                  hora_func_full = hora_func_full + '<strong>Quinta: </strong>'
+                  if(func_ini[4] === null) {
+                    hora_func_full = hora_func_full + 'Não abre' + '<br>'
+                  }
+                  else {
+                    hora_func_full = hora_func_full + func_ini[4].slice(0, -3) + ' às ' + func_fim[4].slice(0, -3) + '<br>'
+                  }
+                  break
+                case 5:
+                  hora_func_full = hora_func_full + '<strong>Sexta: </strong>'
+                  if(func_ini[5] === null) {
+                    hora_func_full = hora_func_full + 'Não abre' + '<br>'
+                  }
+                  else {
+                    hora_func_full = hora_func_full + func_ini[5].slice(0, -3) + ' às ' + func_fim[5].slice(0, -3) + '<br>'
+                  }
+                  break
+                case 6:
+                  hora_func_full = hora_func_full + '<strong>Sábado: </strong>'
+                  if(func_ini[6] === null) {
+                    hora_func_full = hora_func_full + 'Não abre' + '<br>'
+                  }
+                  else {
+                    hora_func_full = hora_func_full + func_ini[6].slice(0, -3) + ' às ' + func_fim[6].slice(0, -3) + '<br>'
+                  }
+                  break
+                default:
+                  break
+              }
+            }
+
+            console.log('hora func full: ' + hora_func_full)
+            data.hora_funcionamento = hora_func_short
+            data.botao_icon = 'fa fa-angle-down'
+
+            filas = dados.filas
+            for(var i = 0; i< filas.length; i++) {
+              vm.$http
+                .get(api(`/filas/${filas[i]}`))
+                .then(function (response) {
+                  console.log(response.data.data)
+                  data.filas = data.filas + '<strong>Fila ' + response.data.data.id + ':</strong>' + '&ensp;'
+                  data.filas = data.filas + response.data.data.descricao + '&ensp;'
+                  data.filas = data.filas + '<i class="fa fa-users" aria-hidden="true"></i> &nbsp;'
+                  if(response.data.data.usuarios_na_fila === null) {
+                    data.filas = data.filas + 0 + '<br>'
+                  }
+                  else {
+                    data.filas = data.filas + response.data.data.usuarios_na_fila.length + '<br>';
+                  }
+                })
+            }
+
+          })
+          .catch(function (err) {
+            console.log(`Error: ${err}`)
+            return false
+          })
       },
       hora_func () {
         if(data.botao_icon === 'fa fa-angle-down') {
-          data.hora_funcionamento = '<strong>Segunda:</strong> 10h às 19h<br><strong>Terça:</strong> 10h às 19h'
+          data.hora_funcionamento = hora_func_full
           data.botao_icon = 'fa fa-angle-up'
         }
         else {
-          this.load_hora_func()
+          data.hora_funcionamento = hora_func_short
+          data.botao_icon = 'fa fa-angle-down'
         }
       },
     },
     mounted () {
-      this.load_hora_func()
+      this.loadPagina()
     },
   }
 </script>
 
 <style lang="scss" scoped>
   @import "../scss/style";
+
+  .hero-body {
+    padding: 0rem 3rem 3rem 3rem;
+  }
 
 </style>
